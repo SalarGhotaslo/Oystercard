cfe697a970b1199b7a9938b8b55d98fa9b096da5
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-13 16:30:37 +0000

    refactored rpec to be more clear and ran rubocop

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,18 @@
+# frozen_string_literal: true
+
 require 'oystercard'
 
 describe Oystercard do
-    context "So that I can top up my oystercard"
-        it 'should start with a balance of zero' do
-            expect(subject.balance).to eq(0)
-        end  
-        it 'should top up the oystercard by the specified amount' do
-            subject.top_up(5)
-            expect(subject.balance).to eq(5)
-        end
+  describe '#Balance' do
+    it 'should start with a balance of zero' do
+      expect(subject.balance).to eq(0)
     end
+  end
+
+  describe '#Top_up' do
+    it 'should top up the oystercard by the specified amount' do
+      subject.top_up(5)
+      expect(subject.balance).to eq(5)
+    end
+  end
+end
