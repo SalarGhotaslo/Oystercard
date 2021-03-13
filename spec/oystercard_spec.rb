377671c05687cf5eeabb8eb43a6d4370270da1d4
Salar Ghotaslo <Salar_@live.co.uk>
2021-03-13 16:14:27 +0000

    added balance of zero

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,8 @@
 require 'oystercard'
 
 describe Oystercard do
-   
+    context "So that I can top up my oystercard"
+        it 'should start with a balance of zero' do
+            expect(subject.balance).to eq(0)
+        end  
     end
